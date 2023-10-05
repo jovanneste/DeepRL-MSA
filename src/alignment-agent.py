9640330c96cc606f7058e8ece63ea3b19b61b5fe
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-10-05 20:58:28 +0100

    new directory

---
+++ src/alignment-agent.py
@@ -0,0 +1,24 @@
+from generator import *
+
+def score(msa):
+    num_sequences = len(msa)
+    alignment_length = len(msa[0])
+    score = 0
+
+    for i in range(alignment_length):
+        for j in range(num_sequences):
+            for k in range(j + 1, num_sequences):
+                if i >= len(msa[j]) or i >= len(msa[k]):
+                    continue  
+
+                char_j = msa[j][i]
+                char_k = msa[k][i]
+
+                if char_j != '_' and char_k != '_':
+                    if char_j == char_k:
+                        score += 1
+    return score
+
+
+sequences = generate_sequence(3, 5)
+print(score(sequences))
---
+++ src/generator.py
@@ -0,0 +1,15 @@
+import random
+import numpy as np
+
+def generate_sequence(n_sequences, length, mutation_prob=0.2, del_prob=0.2):
+    sequences = []
+    aa = ['M', 'A', 'T', 'G', 'Y']
+
+    initial_sequence = [random.choice(aa) for _ in range(length)]
+    sequences.append(initial_sequence)
+
+    for i in range(n_sequences-1):
+        seq_mut = [i if random.random() > mutation_prob else random.choice(aa) for i in initial_sequence]
+        sequences.append([i for i in seq_mut if random.random() > del_prob])
+
+    return np.asarray(sequences)
