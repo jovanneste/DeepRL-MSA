eceaf5a758f657aaab78b288cb4110ef8c6e3d16
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-10-05 20:14:00 +0100

    use numpy

--- seq-generator.py
+++ seq-generator.py
@@ -7,12 +7,9 @@ def generate_sequence(n_sequences, length, mutation_prob=0.2, del_prob=0.2):
 
     initial_sequence = [random.choice(aa) for _ in range(length)]
     sequences.append(initial_sequence)
-    
+
     for i in range(n_sequences-1):
         seq_mut = [i if random.random() > mutation_prob else random.choice(aa) for i in initial_sequence]
         sequences.append([i for i in seq_mut if random.random() > del_prob])
 
-    return sequences
-
-
-generate_sequence(3, 5)
+    return np.asarray(sequences)
