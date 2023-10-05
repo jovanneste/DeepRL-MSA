62a360cc99cf0edb013affc2481ec96473db61ba
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-10-05 20:11:15 +0100

    function added

--- seq-generator.py
+++ seq-generator.py
@@ -1,20 +1,18 @@
 import random
 import numpy as np
 
-length = 5
-n_sequences = 3
-mutation_prob = 0.2
-gap_prob = 0.2
-sequences = []
+def generate_sequence(n_sequences, length, mutation_prob=0.2, del_prob=0.2):
+    sequences = []
+    aa = ['M', 'A', 'T', 'G', 'Y']
 
-aa = ['M', 'A', 'T', 'G', 'Y']
+    initial_sequence = [random.choice(aa) for _ in range(length)]
+    sequences.append(initial_sequence)
+    
+    for i in range(n_sequences-1):
+        seq_mut = [i if random.random() > mutation_prob else random.choice(aa) for i in initial_sequence]
+        sequences.append([i for i in seq_mut if random.random() > del_prob])
 
-initial_sequence = [random.choice(aa) for _ in range(length)]
+    return sequences
 
-sequences.append(initial_sequence)
 
-for i in range(n_sequences-1):
-    seq_mut = [i if random.random() > mutation_prob else random.choice(aa) for i in initial_sequence]
-    sequences.append([i if random.random() > gap_prob else '_' for i in seq_mut])
-
-print(np.asarray(sequences).reshape(n_sequences, length))
+generate_sequence(3, 5)
