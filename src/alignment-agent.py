47c063591b5095db4b73b579217482024356c0b9
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-10-05 22:24:41 +0100

    rl skeleton added

--- src/alignment-agent.py
+++ src/alignment-agent.py
@@ -1,4 +1,5 @@
 from generator import *
+from collections import namedtuple
 
 def score(msa):
     num_sequences = len(msa)
@@ -18,3 +19,42 @@ def score(msa):
                     if char_j == char_k:
                         score += 1
     return score
+
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.memory = []
+
+    def __len__(self):
+        return len(self.memory)
+    
+    def store_experience(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def clear(self):
+        self.memory = []
+    
+
+class DQN():
+    pass
+    # take 2d array -> x,y posistion 
+
+
+def convergence():
+    pass
+    # way to say time to stop actions
+
+
+def getAction(model, state, eplison):
+    pass
+    # epsilon decay either random or np.argmax(tf.nn.softmax(model.predict(state).numpy()[0]))
+
+def performAction(state, action):
+    pass
+    # insert gap 
+    # return new_state, reward, done(bool)
