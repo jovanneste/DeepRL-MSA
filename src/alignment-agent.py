47d53ab79e1e602870f71a69f9a03430bea50bd1
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-10-16 18:31:09 +0100

    new model

--- src/alignment-agent.py
+++ src/alignment-agent.py
@@ -6,7 +6,7 @@ import copy
 import torch
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Flatten, Input
+#from tensorflow.keras.layers import Dense, Flatten, Input
 from tensorflow.keras.applications.resnet50 import preprocess_input
 import torch.nn as nn
 import torch.optim as optim
@@ -104,18 +104,24 @@ class DQN(nn.Module):
         self.input_shape = input_shape
         self.n_actions = n_actions
         self.model = self.build_model()
+        self.model.trainable = True
 
     def build_model(self):
-        model = keras.Sequential([
-            keras.layers.Dense(128, activation='relu', input_shape=self.input_shape),
-            keras.layers.Dense(64, activation='relu'),
-            keras.layers.Dense(self.n_actions, activation='linear')
-        ])
+        model = nn.Sequential(
+            nn.Linear(self.input_shape[0], 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, self.n_actions)
+        )
         return model
+    
+    def params(self):
+        return self.model.parameters()
 
     def forward(self, features):
-        q_values = self.model.predict(features)
-        return np.argmax(q_values), q_values
+        q_values = self.model(features)
+        return torch.argmax(q_values), q_values
 
 def index_to_coords(index):
     return ((index-1)%5, (index-1)//5)
@@ -135,9 +141,11 @@ reduction = epsilon/EPISODES
 def epsilonGreedy(model, features):
     global epsilon, reduction
     if np.random.random() < 1-epsilon:
-        index, q_value = model.forward(features)
+        print("MODEL ACTION")
+        index, q_value = model.forward(torch.tensor(features))
         action = index_to_coords(index)
     else:
+        print("RANDOM ACTION")
         action = (np.random.randint(0, 4), np.random.randint(0, 4))
     if epsilon>0:
         epsilon-=reduction  
@@ -145,45 +153,70 @@ def epsilonGreedy(model, features):
 
 
 model = DQN((2048,), 25)
+LR = 1e-4
 loss_function = nn.MSELoss()
 BATCH_SIZE = 2
 GAMMA = 0.99
-def optimise_model():
+
+optimizer = optim.Adam(model.parameters(), lr=LR)
+def optimise_model3():
+    print("READY TO OPTIMISE MODEL")
     transitions = replay.sample(BATCH_SIZE)
     batch = Transition(*zip(*transitions))
-    q_values = torch.tensor([model.forward(i)[1] for i in batch.features]).view(BATCH_SIZE,25)
 
-    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)))
-    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+    q_values = torch.stack([model.forward(torch.tensor(i))[1] for i in batch.features])
+    q_values = q_values.view(BATCH_SIZE, 25)
+
+    non_final_mask = torch.tensor([s is not None for s in batch.next_state], dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None], dim=0)
+
+    state_batch = torch.cat(batch.state, dim=0)
+    action_batch = torch.cat(batch.action, dim=0)
+    reward_batch = torch.cat(batch.reward, dim=0)
     
-    state_batch = torch.cat(batch.state)
-    action_batch = torch.cat(batch.action)
-    reward_batch = torch.cat(batch.reward)
-    
-    state_action_values = q_values.gather(1, action_batch.unsqueeze(1))
-    next_state_values = torch.zeros(BATCH_SIZE)
-
-    non_final_next_states = non_final_next_states.chunk(2, dim=0)     
+    state_action_values = torch.gather(q_values, 1, action_batch.view(-1, 1))
 
 
-    with torch.no_grad():
-        q_values = torch.tensor([model.forward(get_features(i.numpy()))[1] for i in non_final_next_states]).view(BATCH_SIZE,25)
+    next_state_values = torch.zeros(BATCH_SIZE, dtype=torch.float32)
+
+    # Calculate the expected Q-values for non-final states
+    if non_final_mask.any():
+        non_final_next_states = non_final_next_states.chunk(2, dim=0)
+        q_values = []
+        
+        for state in non_final_next_states:
+            features = get_features(state.numpy())
+            q_values.append(model.forward(torch.tensor(features))[1])
+        q_values = torch.cat(q_values)
         next_state_values[non_final_mask] = q_values.max(1)[0]
-    
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-    
-    print(expected_state_action_values)
-    
 
+    # Calculate the expected Q-values based on the Bellman equation
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch.float()
+
+
+    # Calculate the loss using the Huber loss
+    criterion = torch.nn.SmoothL1Loss()  # Huber loss
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    exit()
+
+    return loss.item()  # Return the loss as a scalar
+
+    
 global replay
 replay = ReplayMemory()
+
+
 def train_alignment_agent(sequences):    
-    optimizer = tf.keras.optimizers.Adam()
     for episode in tqdm.tqdm(range(EPISODES)):
         state = sequences
         replay.clear()
         done = False
-        
+#        optimizer = optim.Adam(model.params(), lr=LR)
         while not done:
             features = get_features(state)
             action = epsilonGreedy(model, features)
@@ -195,11 +228,12 @@ def train_alignment_agent(sequences):
             state = new_state
             
             if done:
-                optimise_model()
+                loss = optimise_model3()
+                print("Loss: ", + str(loss))
+                exit()
                 break
         
     
-
 
 s = generate_sequence(5,5)
 print(s)
