ab152b0e19131dec27b66c516035266dc2d9bc7a
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-10-20 12:27:40 +0100

    return ints

--- src/generator.py
+++ src/generator.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def char_to_int(sequence):
     char_to_number = {chr(65 + i): i + 1 for i in range(26) if chr(65 + i) != '_'}
-    return np.array([[char_to_number.get(c, '_') for c in row] for row in sequence])
+    return np.array([[char_to_number.get(c, '0') for c in row] for row in sequence])
 
 def generate_sequence(n_sequences, length, mutation_prob=0.2, del_prob=0.2):
     sequences = []
@@ -21,5 +21,5 @@ def generate_sequence(n_sequences, length, mutation_prob=0.2, del_prob=0.2):
             sequences.append(seq_del)
 
     padded_array = np.array([row + ['_'] * (length - len(row)) for row in np.asarray(sequences, dtype=object)])
-    return char_to_int(padded_array)
+    return char_to_int(padded_array).astype(int)
 
