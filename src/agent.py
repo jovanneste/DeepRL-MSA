43a2b86bacb32656584192a619fb35d4d8348ba4
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-10-20 12:15:34 +0100

    new agent class

---
+++ src/agent.py
@@ -0,0 +1,31 @@
+from agent_memory import Memory
+from tensorflow.keras.models import Sequential, clone_model
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, Input
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+import numpy as np
+
+class Agent():
+    def __init__(self):
+        self.memory = Memory()
+        self.epsilon = 0.99
+        self.epsilon_decay = 0.9/100000
+        self.gamma = 0.95
+        self.learning_rate = 1e-4
+        self.model = self._build_model()
+        self.model_target = clone_model(self.model)
+        self.total_timesteps = 0
+        
+    def _build_model(self):
+        model = Sequential()
+        model.add(Input((5,5)))
+        model.add(Conv2D(filters = 32,kernel_size = (8,8),strides = 4,data_format="channels_last", activation = 'relu',kernel_initializer = tf.keras.initializers.VarianceScaling(scale=2))
+        model.add(Flatten())
+        model.add(Dense(512,activation = 'relu', kernel_initializer = tf.keras.initializers.VarianceScaling(scale=2)))
+        model.add(Dense(25, activation = 'linear'))
+        optimizer = Adam(self.learn_rate)
+        model.compile(optimizer, loss=tf.keras.losses.Huber())
+        model.summary()
+        print('\nAgent Initialized\n')
+        return model
+                  
