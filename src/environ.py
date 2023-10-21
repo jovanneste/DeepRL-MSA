0e00bcf9419bfcef5a4df322616aad0fe3ee9999
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-10-21 10:08:11 +0100

    start with dummy

--- src/environ.py
+++ src/environ.py
@@ -1,17 +1,29 @@
 import numpy as np
 
+
+def initialise_new_game(agent, state):
+#    process first state maybe
+    agent.memory.store_experience(state, (0,0), 0)
+    
+
+
+
 def take_step(agent):
     agent.total_timesteps += 1
     if agent.total_timesteps % 1000 == 0:
         agent.model.save_weights('recent_weights.hdf5')
         print("Weights saved")
+        
+        action = agent.get_action()
+    
+        
     
     
     
     
     
-    
-def play_episode(agent):
+def play_episode(agent, sequences):
+    initialise_new_game(agent, sequences)
     done = False
     score = 0
     while True:
