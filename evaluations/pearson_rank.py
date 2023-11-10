ab39ebc3c6354379e63656ac592808b79ad29248
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-11-10 14:42:30 +0000

    add fake hist

--- evaluations/pearson_rank.py
+++ evaluations/pearson_rank.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append('../src')
 import scoring
 from scipy.stats import percentileofscore
+import matplotlib.pyplot as plt
     
 def step(state, coords):
     s_list = state.tolist()
@@ -54,3 +55,20 @@ print(percentile)
 # random chosen action 
 
 # repeat for differnent sizes - get percentages HISTOGRAMS
+
+random_per = [15, 59, 35, 23, 66]
+normal_model_per = [55, 77, 34, 81, 81]
+new_model_per = [98, 98, 78, 88, 84]
+
+plt.hist(random_per, bins=5, alpha=0.5, label='Dataset 1', edgecolor='black')
+plt.hist(normal_model_per, bins=5, alpha=0.5, label='Dataset 2', edgecolor='black')
+plt.hist(new_model_per, bins=5, alpha=0.5, label='Dataset 3', edgecolor='black')
+
+# Add labels and title
+plt.title("Histograms of Datasets")
+plt.xlabel("Values")
+plt.ylabel("Frequency")
+plt.legend()
+
+# Show the plot
+plt.show()
