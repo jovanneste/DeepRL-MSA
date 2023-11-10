83412d1994a529d6be62d871feee638d5db7a0c6
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-11-10 14:38:36 +0000

    percentile added

--- evaluations/pearson_rank.py
+++ evaluations/pearson_rank.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 sys.path.append('../src')
 import scoring
-    
+from scipy.stats import percentileofscore
     
 def step(state, coords):
     s_list = state.tolist()
@@ -39,4 +39,18 @@ for iy, ix in np.ndindex(state.shape):
 action_scores = sorted(action_scores.items(), key=lambda x:x[1], reverse=True)
 sorted_action_scores = dict(action_scores)
 
-print(sorted_action_scores)
+input_coord = (0, 0)
+
+# Extract values from the dictionary
+values = list(sorted_action_scores.values())
+
+# Calculate the percentile of the input coordinate
+percentile = percentileofscore(values, sorted_action_scores[input_coord])
+print(percentile)
+
+
+# normal chosen action 
+# new netork chosen action 
+# random chosen action 
+
+# repeat for differnent sizes - get percentages HISTOGRAMS
