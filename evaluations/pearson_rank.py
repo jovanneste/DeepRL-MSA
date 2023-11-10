3e067fb23a5d9188e4dbec5155e04cc0ff349709
Joachim Vanneste <2460800V@student.gla.ac.uk>
2023-11-10 14:29:47 +0000

    rename

---
+++ evaluations/pearson_rank.py
@@ -0,0 +1,42 @@
+import numpy as np
+import sys
+sys.path.append('../src')
+import scoring
+    
+    
+def step(state, coords):
+    s_list = state.tolist()
+    x, y = coords
+    row = s_list[y]
+
+    if row[x] == 0:
+        row.pop(x)
+        row.append(0)
+    else:
+        if 0 in row:
+            row.pop(row.index(0))
+            row.insert(x, 0)
+
+    new_state = np.array(s_list).reshape(state.shape)
+    return scoring.compute_sp_score(new_state)
+
+
+
+state = np.asarray([[0,1,2,3,4],
+                    [1,2,3,4,5],
+                    [1,2,3,4,5],
+                    [1,2,3,4,5],
+                    [1,2,3,4,5]])
+
+
+action_scores = {}
+
+for iy, ix in np.ndindex(state.shape):
+    coords = (ix,iy)
+    action_scores[coords] = step(state, coords)
+
+
+action_scores = sorted(action_scores.items(), key=lambda x:x[1], reverse=True)
+sorted_action_scores = dict(action_scores)
+
+print(sorted_action_scores)
